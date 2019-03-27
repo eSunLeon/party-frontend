5c6c32c7fa156114cf1418ca5d7d121a1e0727de
li361426 <li361426@163.com>
2019-03-27 14:34:25 +0800

    Create bottom.vue

---
+++ src/views/layout/components/bottom.vue
@@ -0,0 +1,37 @@
+<template>
+  <mt-tabbar :fixed="fixed" v-model="selected">
+    <mt-tab-item id="index">
+      <img slot="icon" src="./img/tab.jpg">
+      首页
+    </mt-tab-item>
+    <mt-tab-item id="group">
+      <img slot="icon" src="./img/tab.jpg">
+      党群
+    </mt-tab-item>
+    <mt-tab-item id="user">
+      <img slot="icon" src="./img/tab.jpg">
+      我的
+    </mt-tab-item>
+  </mt-tabbar>
+</template>
+<script>
+export default {
+  name: 'bottom',
+  data() {
+    return {
+      selected: 'index',
+      fixed: false
+    }
+  },
+  watch: {
+    selected(val) {
+      this.$router.push({ name: val })
+    }
+  },
+  created() {
+    this.selected = this.$route.name
+  }
+}
+</script>
+<style>
+</style>
