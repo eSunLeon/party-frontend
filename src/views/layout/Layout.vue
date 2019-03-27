798a81e761c909f83a0558202e8acb7e3c90fa03
li361426 <li361426@163.com>
2019-03-27 14:31:58 +0800

    Create Layout.vue

---
+++ src/views/layout/Layout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <bottom-bar></bottom-bar>
+  </div>
+</template>
+
+<script>
+import bottomBar from './components/bottom'
+export default {
+  name: 'layout',
+  components: {
+    bottomBar
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
