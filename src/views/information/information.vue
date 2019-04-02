087fcb065389dcc3058f985db5370eca6550c028
li361426 <li361426@163.com>
2019-04-02 15:32:45 +0800

    Update information.vue

--- src/views/information/information.vue
+++ src/views/information/information.vue
@@ -1,9 +1,46 @@
 <template>
-  <div>information</div>
+  <div>
+    <div class="tab-bar">
+      <div :key="index" class="tab" v-for="(item, index) in tabList">{{item}}</div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
-  name: 'information'
+  name: 'information',
+  data() {
+    return {
+      tabList: [
+        '我的我的',
+        '我的我的',
+        '我的我的',
+        '我的我的',
+        '我的我的',
+        '我的我的',
+        '我的我的',
+        '我的我的',
+        '我的我的'
+      ]
+    }
+  }
 }
 </script>
+<style lang="less" scoped>
+@r: 50rem;
+.tab-bar {
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: flex;
+  font-size: 26 / @r;
+  overflow-y: hidden;
+  overflow-x: auto;
+  white-space: nowrap;
+  &::-webkit-scrollbar {
+      display: none;
+    }
+  .tab {
+    padding: 12 / @r;
+  }
+}
+</style>
 
