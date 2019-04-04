73ee3be4adc431cae4a38143bde0bb7fdbf49734
li361426 <li361426@163.com>
2019-04-04 13:35:40 +0800

    Update app.js

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -1,15 +1,22 @@
 const app = {
   state: {
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
+    bottomSelect: 0
   },
   mutations: {
     SET_BG: (state, bg) => {
       state.backgroundColor = bg
+    },
+    SET_SELECT: (state, index) => {
+      state.bottomSelect = index
     }
   },
   actions: {
     setBg({ commit }, bg) {
       commit('SET_BG', bg)
+    },
+    setSelect({ commit }, index) {
+      commit('SET_SELECT', index)
     }
   }
 }
