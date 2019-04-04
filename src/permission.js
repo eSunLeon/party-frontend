5cd9386e0279b2dfc464fafebd1f423fc5f81f25
li361426 <li361426@163.com>
2019-04-04 13:35:18 +0800

    Update permission.js

--- src/permission.js
+++ src/permission.js
@@ -4,6 +4,9 @@ import store from './store'
 router.beforeEach((to, from, next) => {
   store.dispatch('setBg', to.meta.bg)
   next()
+  if (!isNaN(to.meta.icon)) {
+    store.dispatch('setSelect', to.meta.icon)
+  }
 })
 
 router.afterEach(() => {
