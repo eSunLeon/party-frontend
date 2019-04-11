636f02f04897f8480fb6ad5b1a8bb6f472306188
li361426 <li361426@163.com>
2019-04-11 16:03:36 +0800

    Update notice.vue

--- src/views/group/components/notice.vue
+++ src/views/group/components/notice.vue
@@ -1,42 +1,54 @@
 <template>
-    <router-link to="/notice" class="notice">
-      <img src="./img/sound.png" class="left"/>
-      【党建】职教中心党总支开展主题党日学习活动!
-      <img src="../../../assets/img/more.png" class="right"/>
-    </router-link>
+  <router-link class="notice" to="/notice">
+    <img class="left" src="./img/sound.png">
+    {{publicNew.title}}
+    <img class="right" src="../../../assets/img/more.png">
+  </router-link>
 </template>
 
 <script>
-  export default {
-    name: 'notice'
+import { publicList } from '@/api/notice'
+export default {
+  name: 'notice',
+  data() {
+    return {
+      publicNew: {}
+    }
+  },
+  created() {
+    publicList().then(res => {
+      if (res.returnCode === '200') {
+        this.publicNew = res.data[0]
+      }
+    })
   }
+}
 </script>
 
 <style lang="less" scoped>
-@r:50rem;
-  .notice {
-    display: block;
-    padding: 23/@r 94/@r;
-    position: relative;
-    color: #333;
-    font-size: 26/@r;
-    white-space:nowrap;
-    overflow: hidden;
-    margin-bottom: 18/@r;
-    .left {
-      position: absolute;
-      left: 40/@r;
-      bottom: 20/@r;
-      width: 35/@r;
-    }
-    .right {
-      position: absolute;
-      right: 40/@r;
-      top: 50%;
-      -webkit-transform: translateY(-50%);
-      transform: translateY(-50%);
-      width: 13/@r;
-    }
-
+@r: 50rem;
+.notice {
+  display: block;
+  padding: 23 / @r 94 / @r;
+  position: relative;
+  color: #333;
+  font-size: 26 / @r;
+  white-space: nowrap;
+  overflow: hidden;
+  margin-bottom: 18 / @r;
+  .left {
+    position: absolute;
+    left: 40 / @r;
+    bottom: 20 / @r;
+    width: 35 / @r;
   }
+  .right {
+    position: absolute;
+    right: 40 / @r;
+    top: 50%;
+    -webkit-transform: translateY(-50%);
+    transform: translateY(-50%);
+    width: 13 / @r;
+  }
+}
 </style>
