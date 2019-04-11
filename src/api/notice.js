34316a9981e1e4f70a5c437fdbb17f05f270ed81
li361426 <li361426@163.com>
2019-04-11 14:26:18 +0800

    Update notice.js

--- src/api/notice.js
+++ src/api/notice.js
@@ -1,7 +1,30 @@
 import request from '@/utils/request'
+/**
+ * 公示公告列表
+ */
 export function publicList() {
   return request({
     method: 'get',
     url: '/dj/informed/notice'
   })
 }
+/**
+ * 公示公告详情
+ */
+
+export function publicDetail(id) {
+  return request({
+    method: 'get',
+    url: '/dj/informed/notice/' + id
+  })
+}
+/**
+ * 党内通报 列表
+ */
+export function innerPartyList() {
+  return request({
+    method: 'get',
+    url: '/dj/informed/notification'
+  })
+}
+
